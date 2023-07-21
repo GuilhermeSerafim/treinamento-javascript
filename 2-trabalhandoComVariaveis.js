ebafd063e79f9f07175fa42e52efa9c96ea25a47
GuilhermeSerafim <guilerstudies@gmail.com>
2023-07-21 19:35:05 -0300

    Entendendo como js interpreta valores

---
+++ 2-trabalhandoComVariaveis.js
@@ -0,0 +1,9 @@
+//Console.log("Trabalhando com variaveis");
+console.log("Trabalhando com variaveis");
+//JS é case sensitive
+
+const idade = 29;
+const nome = "Ricardo";
+
+//Não fazer, pois se você tiver uma aplicação grande, pode gerar problemas...
+//mes = "Janeiro";
---
+++ 3-operacoesAritmeticas.js
@@ -0,0 +1,8 @@
+console.log("Operações Aritméticas");
+//Nesse caso, a operação que tiver * ou /, vai ser priorizada
+console.log(10 + 8 * 2);
+//Por isso usamos parenteses, para o js ler o que queremos priorizar
+console.log((10 + 8) * 2);
+//Concatenação
+console.log("Ano atual: " + 2023);
+console.log("2" + "2");
