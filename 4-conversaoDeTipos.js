e61a7301796ad7de81ba157d16e3f8e6d9934d35
GuilhermeSerafim <guilerstudies@gmail.com>
2023-07-21 19:56:30 -0300

    Mexendo com conversão de tipos

---
+++ 4-conversaoDeTipos.js
@@ -0,0 +1,14 @@
+console.log("Conversão de tipos");
+
+//A operação do mais, virou concatenação, pois temos texto
+console.log("Ano atual: " + 2023);
+console.log("2" + "2");
+
+//Conversão
+console.log(parseInt("2") + parseInt("2"));
+console.log(parseFloat("2.3") + parseFloat("2.2"));
+
+//Em outros tipos de operações, o js interpreta a operação
+console.log("2" / "2");
+//NaN = Not a Number
+console.log("a" / "a");
