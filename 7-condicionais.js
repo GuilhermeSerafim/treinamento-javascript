8ef31ed0bf8f0fb4f44a9cbb46c5aed363aeeb5d
GuilhermeSerafim <guilerstudies@gmail.com>
2023-07-24 17:12:42 -0300

    Finalizando operadores lógicos encadeados e ifs encadeados

--- 7-condicionais.js
+++ 7-condicionais.js
@@ -8,17 +8,32 @@ const listaDeDestinos = new Array(
 
 const idadeComprador = 17;
 const estaAcompanhado = true;
+const temPassagemComprada = true;
 
-if(idadeComprador >= 18) {
-    console.log("Comprador maior de idade");
+// if (idadeComprador >= 18) {
+//     console.log("Comprador maior de idade");
+//     listaDeDestinos.splice(1, 1);
+// } else if (estaAcompanhado == true) {
+//     console.log("Está acompanhado");
+//     listaDeDestinos.splice(1, 1);
+// } else {
+//     console.log("Não é maior de idade e não posso vender");
+// }
+
+if (idadeComprador >= 18 || estaAcompanhado == true) {
+    console.log("\nPassagem comprada");
     listaDeDestinos.splice(1, 1);
-} else if (estaAcompanhado == true) {
-        console.log("Está acompanhado");
-        listaDeDestinos.splice(1, 1);
-    } else {
-        console.log("Não é maior de idade e não posso vender");
-    }
+} else {
+    console.log("Não posso vender");
+}
 
-console.log(listaDeDestinos);
+console.log("\nListas de destino:\n" + listaDeDestinos);
+
+console.log("Embarque: \n");
+if(idadeComprador >=18 && temPassagemComprada) {
+    console.log("Pode embarcar");
+} else {
+    console.log("Não pode embarcar");
+}
 // //Podemos retornar um valor bollean pelo console
 // console.log(idadeComprador >= 18);
